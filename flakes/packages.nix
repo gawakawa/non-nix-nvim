516fd8c334c86752dc335ff08c7f562f3b75a50e
gawakawa <mashurakiryu@icloud.com>
2026-06-13 20:10:12 +0900

    🎉 Initial commit

---
+++ flakes/packages.nix
@@ -0,0 +1,25 @@
+{ flake-parts-lib, ... }:
+{
+  options.perSystem = flake-parts-lib.mkPerSystemOption (
+    { lib, ... }:
+    {
+      options.ciPackages = lib.mkOption {
+        type = lib.types.listOf lib.types.package;
+        default = [ ];
+        description = "Packages for CI environment";
+      };
+    }
+  );
+
+  config.perSystem =
+    { config, pkgs, ... }:
+    {
+      ciPackages = with pkgs; [
+      ];
+
+      packages.ci = pkgs.buildEnv {
+        name = "ci";
+        paths = config.ciPackages;
+      };
+    };
+}
