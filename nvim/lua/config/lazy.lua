38d5b5d5cafffb04e61c1c8c758861dbfb1f137f
gawakawa <mashurakiryu@icloud.com>
2026-06-14 02:19:10 +0900

    Revert "✨ Add LSP, completion, and snippet plugins"
    This reverts commit a2d3a3fef99fb9cc8a4899b37bad8b621f80bd28.

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,7 +1,3 @@
--- Disable netrw (replaced by fyler)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
